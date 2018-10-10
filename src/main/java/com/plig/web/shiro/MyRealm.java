bb4dbaec784727e898617b3127af6d1eaaa24ad8
zy <601819456@qq.com>
2018-10-10 21:05:39 +0800

    20181010905

---
+++ src/main/java/com/plig/web/shiro/MyRealm.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: MyRealm
+ * Author:   邦哥的电脑
+ * Date:     2018-10-10 20:54
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.plig.web.shiro;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 邦哥的电脑
+ * @create 2018-10-10
+ * @since 1.0.0
+ */
+public class MyRealm {
+
+}
+
+
