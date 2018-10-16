b1b050a18b482a8b18b200c93b173550e5a6cdf7
wxb <wxb_1214@plig.com>
2018-10-16 09:34:13 +0800

    猜你喜欢添加，10-16，主要功能增删改查，展示所有

--- ShiroMy/src/main/java/com/zy/shirozy/mapper/LikeMapper.java
+++ ShiroMy/src/main/java/com/zy/shirozy/mapper/LikeMapper.java
@@ -2,17 +2,27 @@ package com.zy.shirozy.mapper;
 
 
 import com.zy.shirozy.domain.Like;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 public interface LikeMapper {
-    int deleteByPrimaryKey(Integer id);
 
-    int insert(Like record);
+    @Delete("delete from t_like where id = #{id}")
+    int deleteById(Integer id);
 
-    int insertSelective(Like record);
+    @Insert("insert into t_like (aid) values (#{aid})")
+    int insert(Like like);
+    @Select("select * from t_like where id=#{id}")
+    Like selectById(Integer id);
 
-    Like selectByPrimaryKey(Integer id);
+    //查询所有
+    @Select("select * from t_like")
+    List<Like> queryAll();
 
-    int updateByPrimaryKeySelective(Like record);
-
-    int updateByPrimaryKey(Like record);
+    @Update("update t_like set aid = #{aid} where id = #{id}")
+    int updateById(Like like);
 }
